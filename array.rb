8a49c7aa30aad6c912b54bc82e17ba1f8dd32952
jahnavisananse <jahnavisananse@gmail.com>
2021-06-30 11:37:44 +0530

    array's in ruby

---
+++ array.rb
@@ -0,0 +1,21 @@
+
+friends = ['abc','xyz','pqr']
+friend = Array['abc','xyz','pqr']
+
+puts friends[1]
+puts friends
+puts friend[0]
+puts friend[-1]
+friend[0] = 'jjj'
+puts friend
+puts friends.include? 'abc'
+puts friends.reverse()
+puts friends.sort()
+
+
+
+frnds = Array.new
+frnds[0] = 'a'
+frnds[5] = 'x'
+puts frnds
+
