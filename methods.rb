71e60b922d2fb766a342f251b3bde24524c21ad4
jahnavisananse <jahnavisananse@gmail.com>
2021-06-30 11:45:16 +0530

    minor change in methods file

--- methods.rb
+++ methods.rb
@@ -20,3 +20,15 @@ def cube(num)
   return num * num * num
 end
 puts cube(3)
+
+
+def max(n1,n2,n3)
+  if n1>=n3 and n1>=n3
+    return n1
+  elsif n2>=n1 and n2>=n3
+    return n2
+  else
+    return n3
+  end
+end
+puts max(2,4,9)
